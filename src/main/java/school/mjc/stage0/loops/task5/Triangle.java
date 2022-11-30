3acfb42c6cb5a84f028a0b82770097dfe45dcd8c
VarvaraKrestyanova <krestyanovav@gmail.com>
2022-11-30 16:06:32 +0800

    triangle, hourglass

--- src/main/java/school/mjc/stage0/loops/task5/Triangle.java
+++ src/main/java/school/mjc/stage0/loops/task5/Triangle.java
@@ -3,5 +3,12 @@ package school.mjc.stage0.loops.task5;
 public class Triangle {
     public void printTriangle(int cathetusLength) {
 
+        for (int i = 0; i < cathetusLength; i++) {
+            for (int j = 0; j < cathetusLength; j++) {
+                System.out.print(j <= i ? 8 : " ");
+            }
+            System.out.println();
+        }
     }
+
 }
